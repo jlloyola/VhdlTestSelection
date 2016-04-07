03772e38729c35b18a3772e81a59940bf3a01d15
jlloyola <joseloylar@hotmail.com>
2016-04-06 22:32:48 -0500

    Extract File Paths from VUnit
    Added steps to extract VHDL files from the VUnit API. Wrote a
    pseudo-algorithm to do test selection. Also documented our potential
    options to do test selection.

--- RunProject.py
+++ RunProject.py
@@ -5,5 +5,38 @@ root = dirname(__file__)
 
 ui = VUnit.from_argv()
 lib = ui.add_library("lib")
-lib.add_source_files(join(root, "./Source/*.vhd"))
+SourcePath = "Source"
+lib.add_source_files(join(root, SourcePath, "*.vhd"))
+
+# Get all the .vhd source files
+SrcFiles = ui.get_source_files('*.vhd')
+# Create a list with the files to be tested
+PrjFiles = []
+for file in SrcFiles:
+    if SourcePath in file.name:
+        print (file.name)
+        PrjFiles.append(file.name)
+        # Verify checksum
+
+        # If != Append to list of test to run
+    #......
+print (PrjFiles)
+
+#   is to pass them as arguments).
+# + Create a separate parent script that calls RunProject.py with the tests
+#   two run as arguments. This might imply that we need to do the checksum test
+#   without VUnit. This option sounds better.
+
+# FifoAluModule = lib.get_source_file("Source/StdFifo.vhd")
+# Dependencies = ui.get_compile_order(FifoAluModule)
+# for i in Dependencies:
+    # print (i.name)
 ui.main()
+# testEntities = ui._create_tests(None)
+# for i in testEntities:
+    # print (i.name)
+    # print (i.test_cases)
+    # for j in i.test_cases:
+        # print (j)
+# report = TestReport(printer=ui._printer)
+# ui._run_test(testEntities, report)

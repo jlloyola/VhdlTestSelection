76faa2973af99739b7695292c106996f85fcdd6a
Guillermo Leon <gleonoliva@gmail.com>
2016-04-09 01:22:39 -0500

    Renamed HashCalculator to HashManager to better represent its purpose. Created a class for that, that way we can have several sources of files for reading/computing/saving at a time.

---
+++ HashManager.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+import hashlib
+import os
+import fnmatch
+from os import path
+
+class HashManager :
+    """
+    Class that manages computing a hash from object files generated from source files. 
+    As well as saving those computed hashes into an specified directory.
+    """
+    
+    # Constructor
+    def __init__(self, lib_name, obj_dir, src_dir, saved_hash_dir, hash_ext, obj_ext):
+        self.lib_name = lib_name
+        self.obj_dir = obj_dir
+        self.src_dir = src_dir
+        self.saved_hash_dir = saved_hash_dir
+        self.hash_ext = hash_ext
+        self.obj_ext = obj_ext
+
+    # Private Methods
+    def _get_hash_file_path(self, source_file):
+        """
+        Returns the file path that contains the hash associated with source_file. 
+        """
+        head, tail = path.split(source_file)
+        hash_file_name = '{}.{}'.format(path.splitext(tail)[0], self.hash_ext)
+        hash_file_path = path.join(self.saved_hash_dir, head, hash_file_name)
+        return path.normpath(hash_file_path)
+
+    def _get_object_file_path(self, source_file):
+        """
+        Returns the file path to the object file associated with source_file.
+        """
+        head, tail = path.split(source_file)
+        object_file_name = '{}.{}'.format(path.splitext(tail)[0], self.obj_ext)
+        object_file_path = path.join(self.obj_dir, self.lib_name, object_file_name) 
+        return path.normpath(object_file_path)
+
+    # Public Methods
+    def get_saved_hash(self, source_file):
+        """
+        Gets the saved hash associated with source_file. Return None if no
+        hash was saved previously.
+        """
+        hash_file_path = self._get_hash_file_path(source_file)
+        if (path.isfile(hash_file_path)):
+            with open(hash_file_path, 'r') as afile:
+                return afile.read()
+        # Not found
+        return None
+
+    def get_file_hash(self, source_file):
+        """
+        Computes the hash of the object file associated with source_file.
+        """
+        file_path = self._get_object_file_path(source_file)
+        # on windows object files are not generated
+        # so we use source files instead.
+        file_path = file_path if os.path.exists(file_path) else source_file
+        m = hashlib.md5()
+        with open(file_path, 'rb') as afile:
+            buf = afile.read()
+            m.update(buf)
+        return m.hexdigest()
+
+    def save_hash(self, source_file, hash):
+        """
+        Saves a computed hash into a file that mimics source_file relative path 
+        hierarchy, but in saved_hash_directory. Creates all needed directories.
+        """
+        hash_file = self._get_hash_file_path(source_file)
+        # Create directory structure if needed.
+        os.makedirs(os.path.dirname(hash_file), exist_ok=True)
+        with open(hash_file, 'w+') as afile:
+            afile.write(hash)
--- VhdlTestSelection.py
+++ VhdlTestSelection.py
@@ -5,17 +5,19 @@ from os import path
 import fnmatch
 import sys, logging
 import subprocess
-from HashCalculator import get_saved_hash, get_file_hash, save_hash
+from HashManager import HashManager 
 
 # Uncomment this line to print the debug log
 # logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 # Location of the source files to be processed
 prj_dir = 'Source'
+hash_manager = HashManager('lib', './vunit_out/ghdl/libraries', './Source', './saved_hashes', 'xtc_hash', 'o')
 
 # Compile the VHDL project to ensure the files are up to date
 try:
-    subprocess.check_call("RunVUnit.py --compile", shell=True)
+    runVUnitPath = path.normpath(path.abspath("./RunVUnit.py"))
+    subprocess.check_call(runVUnitPath + " --compile", shell=True)
 except:
     print ("VUnit compilation failed")
     sys.exit(2)
@@ -27,30 +29,30 @@ hash_dict = {}
 # Contains the path of the files whose will be executed.
 file_path_dict = {}
 # Search the workspace for all VHDL files
-file_list = os.listdir(prj_dir)
-for file in file_list:
-    if fnmatch.fnmatch(file, '*.vhd'):
-        # Obtain the previous and current hashes from the file
-        file_path = path.join(prj_dir, file)
-        previous_hash = get_saved_hash(file_path)
-        current_hash = get_file_hash(file_path)
-        logging.debug('Source file: {}'.format(file_path))
-        logging.debug('Previous Hash: {}'.format(previous_hash))
-        logging.debug('Current Hash: {}'.format(current_hash))
-        # Add the file to files_to_test when the hashes do not match,
-        # store its current hash in the hash dictionary, and store its path
-        # into a dictionary too.
-        if previous_hash != current_hash:
-            files_to_test.append(file)
-            hash_dict[file] = current_hash
-            file_path_dict[file] = file_path
+for root, dirs, files in os.walk(prj_dir):
+    for file in files:
+        if fnmatch.fnmatch(file, '*.vhd'):
+            # Obtain the previous and current hashes from the file
+            file_path = path.join(prj_dir, file)
+            previous_hash = hash_manager.get_saved_hash(file_path)
+            current_hash = hash_manager.get_file_hash(file_path)
+            logging.debug('Source file: {}'.format(file_path))
+            logging.debug('Previous Hash: {}'.format(previous_hash))
+            logging.debug('Current Hash: {}'.format(current_hash))
+            # Add the file to files_to_test when the hashes do not match,
+            # store its current hash in the hash dictionary, and store its path
+            # into a dictionary too.
+            if previous_hash != current_hash:
+                files_to_test.append(file)
+                hash_dict[file] = current_hash
+                file_path_dict[file] = file_path
 
 logging.debug(files_to_test)
 
 if files_to_test != []:
     # Execute the tests for the files with whose hash mismatched
     for f in files_to_test:
-        vunit_command = "RunVUnit.py "
+        vunit_command = path.normpath(path.abspath("./RunVUnit.py"))
         # Remove the .vhd extension, make all file names lower case (required by
         # VUnit) and, concatenate it to the vunit_command
         test_entity = str(f)
@@ -61,14 +63,10 @@ if files_to_test != []:
             subprocess.check_call(vunit_command, shell=True)
             # Update the hash file when the tests complete successfully
             try:
-                save_hash(file_path_dict[f], hash_dict[f])
+                hash_manager.save_hash(file_path_dict[f], hash_dict[f])
             except:
                 print ("save_hash failed")
         except:
             print ("VUnit tests failed")
 else:
     print ("Files have not changed. No tests were required to run.")
-
-
-
-
